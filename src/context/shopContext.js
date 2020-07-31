2057ef58ffd5610a9e7e8ffb8ba329c936e0a094
mark-scholes <innominatus15@outlook.com>
2020-07-31 18:00:02 +0100

    fixed issue with login data that was preventing app to connect with backend

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -5,7 +5,7 @@ const ShopContext = React.createContext();
 
 const client = Client.buildClient({
   storefrontAccessToken: "dd4d4dc146542ba7763305d71d1b3d38",
-  domain: "graphql.myspotify.com",
+  domain: "graphql.myshopify.com",
 });
 
 class ShopProvider extends Component {
@@ -16,6 +16,25 @@ class ShopProvider extends Component {
     isCartOpen: false,
     test: "test",
   };
+
+  componentDidMount() {
+    this.createCheckout();
+  }
+
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    console.log(checkout);
+  };
+
+  addItemsToCart = async (variantId, quantity) => {};
+
+  fetchAllProducts = async () => {};
+
+  fetchProductById = async (id) => {};
+
+  closeCart = () => {};
+
+  openCart = () => {};
 
   render() {
     return (
