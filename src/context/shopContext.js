c26f9c38c45d2ba48d0a767dade5f0515cf8fccc
mark-scholes <innominatus15@outlook.com>
2020-07-31 20:25:32 +0100

    Added the layout and functionality for the product page. Each product when clicked on will now display on a seperate page where the user can find out more details and add to cart

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -27,7 +27,7 @@ class ShopProvider extends Component {
     console.log(checkout);
   };
 
-  addItemsToCheckout = async (variantId, quantity) => {
+  addItemToCheckout = async (variantId, quantity) => {
     const lineItemsToAdd = [
       {
         variantId,
@@ -69,7 +69,7 @@ class ShopProvider extends Component {
             fetchProductById: this.fetchProductById,
             closeCart: this.closeCart,
             openCart: this.openCart,
-            addItemsToCheckout: this.addItemsToCheckout,
+            addItemToCheckout: this.addItemToCheckout,
           }}
         >
           {this.props.children}
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/shopContext";
+import { Text, Div, Button, Row, Col, Container } from "atomize";
 
 const ProductPage = () => {
-  return <div>Product Page</div>;
+  let { id } = useParams();
+  const { fetchProductById, addItemToCheckout, product } = useContext(
+    ShopContext
+  );
+
+  useEffect(() => {
+    fetchProductById(id);
+    return () => {};
+  }, [fetchProductById, id]);
+  if (!product.title) return <div>Loading</div>;
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Div
+            bgImg={product.images[0].src}
+            bgSize="cover"
+            bgPos="center center"
+            h="40rem"
+          />
+        </Col>
+        <Col>
+          <Text>{product.title}</Text>
+          <Text>£{product.variants[0].price}</Text>
+          <Button onClick={() => addItemToCheckout(product.variants[0].id, 1)}>
+            Add To Cart
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProductPage;
